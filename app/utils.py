c5ddc37e4c9b5986ddfefcb42eb2bf5f3671d3a8
Teonten <Teonten.uk@gmail.com>
2024-12-15 20:30:33 +0100

    after much striving and headaches, this one seems to be the final saviour we have been waiting for.. at least

--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,7 @@ import logging
 import time
 from config.settings import NODE_CONF
 # Setup logging
-logging.basicConfig(filename=f"logs/{NODE_CONF['hostname']}.log", level=logging.INFO)
+logging.basicConfig(filename=f"/usr/local/dva261-common/logs/{NODE_CONF['hostname']}.log", level=logging.INFO)
 
 def log_message(message):
     logging.info(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {message}")
--- config/settings.py
+++ config/settings.py
@@ -4,8 +4,17 @@ import gpiozero
 CTRL_HOST = "172.23.19.1" #"10.42.0.1"
 CTRL_PORT = 443
 HOSTNAME = "ultraut"
-HOST_IP = "10.132.136.7"
+HOST_IP = "172.23.19.4" #"10.42.0.151" #"10.132.136.7"
 HOST_PORT = 2443
+
+# for testing:
+#CTRL_HOST = "192.168.0.5" #"172.23.19.1"
+#CTRL_PORT = 443
+#HOSTNAME = "ultraut"
+#HOST_IP = "192.168.0.4" #"10.132.136.7"
+#HOST_PORT = 2443
+
+
 
 # API Key for Node
 API_KEY = "key_" + HOSTNAME
@@ -24,7 +33,7 @@ NODE_CONF = {
 # Define node communication time synchronization
 SYNC_HB_DELAY = 0.05
 ACK_TIMEOUT = 1
-MAX_RETRIES = 2
+MAX_RETRIES = 20000 ### changed to 20000
 RETRY_DELAY = 1
 
 COM_INT = {
@@ -37,16 +46,19 @@ COM_INT = {
 
 #Define ultrasonic communcation:
 ULTRASONIC = gpiozero.DistanceSensor(echo=17,trigger=4,max_distance=1.0,threshold_distance=0.5,queue_len=3)
-BUTTON = gpiozero.Button(27, active_state=None, pull_up=False, bounce_time=0.02)
+YELLOW_LED = gpiozero.OutputDevice(22, active_high=True, initial_value=False)
 RED_LED = gpiozero.OutputDevice(23, active_high=True, initial_value=False)
 GREEN_LED = gpiozero.OutputDevice(24, active_high=True, initial_value=False)
-YELLOW_LED = gpiozero.OutputDevice(22, active_high=True, initial_value=False)
+BUTTON = gpiozero.Button(27, active_state=None, pull_up=False, bounce_time=0.02)
 
 # In-memory node status
 NODE_STATUS = {
     "hostname": HOSTNAME,
     "next": None,
     "button": False,
+    "yellow_led": False,
+    "red_led": True,
+    "green_led": None,
     "ultrasonic": False,
     "door_lock": {
         "lock": False,
